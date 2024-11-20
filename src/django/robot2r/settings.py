3b6f98e602990b33f126f081a8827bf294325a74
cdamayab <cdamayab@gmail.com>
2024-11-20 03:11:14 -0500

    robot2r django: directory structure changed -new user added

--- src/django/robot2r/settings.py
+++ src/django/robot2r/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=resqe87m(tkxn5ozcj)i^t4iih0io8x)27%p^*z*0ib5p9av5'
+SECRET_KEY = 'django-insecure-wow67pad*3mo4x90#_#m=q9_9*0a#r2_zkt8_cmb#2n+a=2es@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'robot2r_control.urls'
+ROOT_URLCONF = 'src.django.robot2r.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'robot2r_control.wsgi.application'
+WSGI_APPLICATION = 'src.django.robot2r.wsgi.application'
 
 
 # Database
