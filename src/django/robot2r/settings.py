851e7edd473f937b97b42179ea7127b9859a5bcf
cdamayab <cdamayab@gmail.com>
2026-05-09 12:17:19 -0500

    changes

--- src/django/robot2r/settings.py
+++ src/django/robot2r/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'src.django.robot2r.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'src/django/robot2r/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
